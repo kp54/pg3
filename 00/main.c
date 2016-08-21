2e9007ce40301d321f2c006aaa7a846ed95bc20c
kp54 <kangpang65@gmail.com>
2016-08-21 19:31:15 +0900

    第5回分までを統合.

--- 00/main.c
+++ 00/main.c
@@ -2,14 +2,21 @@
 #include <stdlib.h>
 
 #include "node.h"
+#include "node2.h"
 
 extern int print_menu(void);
 extern int load_dicdata(char*, char [][256], char [][256]);
 extern void print_array(char [][256], int);
+
 extern void print_list(node*);
 extern node* create_node(char*, char*);
 extern void concat_list(node*, node*);
 extern void close_list(node*);
+
+extern void print_tree(node2*);
+extern node2* create_node2(char*, char*);
+extern void insert_tree(node2*, node2*);
+extern void close_tree(node2*);
 
 int main(void)
 {
@@ -21,6 +28,7 @@ int main(void)
 	char jpn[20][256];
 		for(i=0;i<20;i++) jpn[i][0]='\0';
 	node* init=create_node("","");
+	node2* root=NULL;
 	
 	while(1){
 		menu_num=print_menu();
@@ -51,26 +59,50 @@ int main(void)
 					break;
 					
 				case 4:
-					if(!init->next){
-						printf("標準出力する連結リストは存在しない\n");
-					}else{
+					if(init->next){
 						printf("%-16s\t%-8s\t%-8s\t%-16s\n", "アドレス","eng","jpn","next");
 						print_list(init->next);
+					}else{
+						printf("標準出力する連結リストは存在しない\n");
 					}
 					break;
 				
 				case 5:
+					if(count1==-1){
+						printf("保存に失敗\n");
+					}else{
+						if(count1==0) break;
+						root=create_node2(eng[0],jpn[0]);
+						for(i=1;i<count1;i++)
+							insert_tree(create_node2(eng[i],jpn[i]),root);
+					}
+					break;
+				
 				case 6:
+					if(root){
+						printf("%-16s\t%-8s\t%-8s\t%-16s\t%-16s\n", "アドレス","eng","jpn","left","right");
+						print_tree(root);
+					}else{
+						printf("標準出力する二分木は存在しない\n");
+					}
+					break;
+					
 				case 7:
 				case 8:
 					printf(">>未実装\n");
 					break;
 				
 				case 9:
-					if(!init->next){
-						printf("削除する連結リストは存在しない\n");
-					}else{
+					if(init->next){
 						close_list(init);
+					}else{
+						printf("削除する連結リストは存在しない\n");
+					}
+					
+					if(root){
+						close_tree(root);
+					}else{
+						printf("削除する二分木は存在しない\n");
 					}
 					
 					printf(">>終了\n");
